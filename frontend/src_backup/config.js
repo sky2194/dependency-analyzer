5b23983ba167983d214c47f39102a41c904b60a5
sky2194 <koutilya99@gmail.com>
2026-05-06 22:57:12 -0400

    chore: remove src_backup

--- frontend/src_backup/config.js
+++
@@ -1,7 +0,0 @@
-// Production: set VITE_API_URL to the DigitalOcean backend origin.
-// Development: leave empty so Vite proxies /api to localhost:5000.
-const API_BASE = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL.replace(/\/$/, '')
-  : ''
-
-export default API_BASE
